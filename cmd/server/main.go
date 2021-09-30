8f886942ce174916c37f2df2b64e392a27d665de
M1racle.Hao <crazyhl@163.com>
2021-09-30 23:08:38 +0800

    修改正式运行调用

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	go_http_application_with_tdd "github.com/crazyhl/go-http-application-with-tdd"
+	gohttpapplicationwithtdd "github.com/crazyhl/go-http-application-with-tdd"
 	"log"
 	"net/http"
 )
 
 func main() {
-	store := go_http_application_with_tdd.InMemoryPlayerStore{}
-	server := &go_http_application_with_tdd.PlayerServer{Store: &store}
+	server := &gohttpapplicationwithtdd.PlayerServer{Store: gohttpapplicationwithtdd.NewInMemoryPlayerStore()}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
