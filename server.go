e6180f613e0d3bcccc56db9a8842b73e192dc4f8
M1racle.Hao <crazyhl@163.com>
2021-09-30 22:42:40 +0800

    抽象代码

--- server.go
+++ server.go
@@ -16,16 +16,17 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
 	switch r.Method {
 	case http.MethodGet:
-		p.showScore(w, r)
+		p.showScore(w, r, player)
 	case http.MethodPost:
-		p.processWin(w, r)
+		p.processWin(w, r, player)
 	}
 }
 
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request, player string) {
 	score := p.Store.GetPlayerScore(player)
 
 	if score == 0 {
@@ -35,8 +36,7 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request, player string) {
 	p.Store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
