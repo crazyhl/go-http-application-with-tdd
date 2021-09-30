8132d76802b7fac1c5e3e398055023845db9d380
M1racle.Hao <crazyhl@163.com>
2021-09-30 19:23:01 +0800

    增加根据用户名获取分数的方法和测试代码

--- server.go
+++ server.go
@@ -3,8 +3,22 @@ package go_http_application_with_tdd
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	fmt.Fprint(w, GetPlayerScore(player))
+}
+
+func GetPlayerScore(player string) string {
+	if player == "Pepper" {
+		return "20"
+	}
+
+	if player == "Floyd" {
+		return "10"
+	}
+	return ""
 }
