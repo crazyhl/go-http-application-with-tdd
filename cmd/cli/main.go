7423dfc8b649cf7c1498c0b243b74f1242d959ea
M1racle.Hao <crazyhl@163.com>
2021-10-05 12:20:28 +0800

    修改  main ，补全最新参数

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	go_http_application_with_tdd "github.com/crazyhl/go-http-application-with-tdd"
+	poker "github.com/crazyhl/go-http-application-with-tdd"
 	"log"
 	"os"
 )
@@ -13,13 +13,13 @@ func main() {
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
 
-	store, close, err := go_http_application_with_tdd.FileSystemPlayerStoreFromFile(dbFileName)
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer close()
 
-	game := go_http_application_with_tdd.NewCLI(store, os.Stdin, go_http_application_with_tdd.BlindAlerterFunc(go_http_application_with_tdd.StdOutAlerter))
+	game := poker.NewCLI(store, os.Stdin, poker.BlindAlerterFunc(poker.StdOutAlerter))
 	game.PlayPoker()
 }
